7251785c05d3ddd98180e5b4dab43917b7124f8f
Ben Bridle <bridle.benjamin@gmail.com>
2020-07-20 07:56:26 +1200

    Remove unneeded secret key from config.py

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "secret-key"
     SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://doctrine:doctrine@localhost/doctrine_game"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
